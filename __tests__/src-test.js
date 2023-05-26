b5bb5e734b885642a118857178ec5b8b34265288
鈴木詠斗 <suzuki8@mba8.local>
2023-05-27 02:46:13 +0900

    add render-test

--- __tests__/src-test.js
+++ __tests__/src-test.js
@@ -16,6 +16,59 @@ describe('createElement', () => {
   });
 });
 
+describe('render', () => {
+  let container = null;
+ 
+  beforeEach(() => {
+    const dom = new JSDOM('<!doctype html><html><body></body></html>');
+    global.document = dom.window.document;
+    container = document.createElement('div');
+  });
 
+  afterEach(() => {
+    delete global.document;
+    container = null;
+  });
+
+  test('render element with props and appends it to container', () => {
+    let element = {
+      type: 'div',
+      props: {
+        id: 'id',
+        className: 'class',
+        type: 'type',
+        value: 'こんにちは',
+        children: [],
+      },
+    };
+    const container = document.createElement('div');
+    render(element, container);
+
+    const divElement = container.firstChild;
+
+    expect(divElement.tagName).toBe('DIV');
+    expect(divElement.id).toBe('id');
+    expect(divElement.className).toBe('class');
+    expect(divElement.type).toBe('type');
+    expect(divElement.value).toBe('こんにちは');
+  });
+
+  test('render element with child and appends it to container', () => {
+    const childElement = createElement('child', { children: [] });
+    const parentElement = {
+      type: 'parent',
+      props: {
+        children: [childElement],
+      },
+    };
+    const container = document.createElement('div');
+    render(parentElement, container);
+
+    const divElement = container.firstChild;
+
+    expect(divElement.tagName).toBe('PARENT');
+    expect(divElement.firstChild.tagName).toBe('CHILD');
+  });
+});
 
 
--- src.js
+++ src.js
@@ -1,3 +1,13 @@
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT', // 特別なテキストノード
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
 export function createElement(type, props, ...children) {
   return {
     type,
@@ -8,5 +18,22 @@ export function createElement(type, props, ...children) {
       ),
     },
   };
+}
+
+export function render(element, container) {
+  const dom =
+    element.type == 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child) => render(child, dom));
+  container.appendChild(dom);
 }
 
